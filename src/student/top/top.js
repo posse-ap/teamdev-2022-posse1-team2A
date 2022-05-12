f2e000a677a7284c6f8c7d88b9c763d490b5914d
NanamiOzeki <nanasmile7373@gmail.com>
2022-05-13 00:09:03 +0900

    画像戻るようにした。ドロップダウンと矢印の向き変えた

--- src/student/top/top.js
+++ src/student/top/top.js
@@ -1,1 +1,16 @@
 'use strict'
+let down= document.getElementsByClassName("fa-sort-down");
+
+document.getElementById("sort_title_gyoukai").onclick = function() {
+    document.getElementById('sort_option_gyoukai_container').style.display = "block";
+    down[0].classList.add("rotate");
+    
+  };
+document.getElementById("sort_title_area").onclick = function() {
+    document.getElementById('sort_option_area_container').style.display = "block";
+    down[10].classList.add("rotate");
+  }; 
+document.getElementById("sort_title_picky").onclick = function() {
+    document.getElementById('sort_option_picky_container').style.display = "block";
+    down[17].classList.add("rotate");
+  }; 

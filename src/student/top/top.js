f398dc51735c466181b7ea97811d4e3c3e835ade
NanamiOzeki <nanasmile7373@gmail.com>
2022-05-26 21:36:19 +0900

    アコーディオンメニュー 未完成

--- src/student/top/top.js
+++ src/student/top/top.js
@@ -1,21 +1,32 @@
 'use strict'
-let down= document.getElementsByClassName("fa-sort-down");
+// let down= document.getElementsByClassName("fa-sort-down");
 
-document.getElementById("sort_title_gyoukai").onclick = function() {
-    document.getElementById('sort_section_gyoukai_container').style.display = "block";
-    down[0].classList.add("rotate");
+// document.getElementById("sort_title_gyoukai").onclick = function() {
+//     document.getElementById('sort_section_gyoukai_container').style.display = "block";
+//     down[0].classList.add("rotate");
     
-  };
-document.getElementById("sort_title_area").onclick = function() {
-    document.getElementById('sort_section_area_container').style.display = "block";
-    down[1].classList.add("rotate");
-  }; 
-document.getElementById("sort_title_picky").onclick = function() {
-    document.getElementById('sort_section_picky_container').style.display = "block";
-    down[2].classList.add("rotate");
-  }; 
+//   };
+// document.getElementById("sort_title_area").onclick = function() {
+//     document.getElementById('sort_section_area_container').style.display = "block";
+//     down[1].classList.add("rotate");
+//   }; 
+// document.getElementById("sort_title_picky").onclick = function() {
+//     document.getElementById('sort_section_picky_container').style.display = "block";
+//     down[2].classList.add("rotate");
+//   }; 
 
+  const menu = document.querySelectorAll(".js-menu");
 
+  function toggle(event) {
+    console.log(event);
+    // const content = document.querySelector(`sort_section_container_${}`);
+    // this.classList.toggle("is-active");
+    // content.classList.toggle("is-open");
+  }
+  
+  for (let i = 0; i < menu.length; i++) {
+    menu[i].addEventListener("click", toggle);
+  }
 
 //   もう一回押したら閉じるようにする
 

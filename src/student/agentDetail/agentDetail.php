fb87480f0e2846cc8a0cee77dbd04f1e365a24d0
ugg <ugkirin@gmail.com>
2022-05-29 10:13:10 +0900

    エージェント押したら詳細いけちゃうぜ

--- src/student/agentDetail/agentDetail.php
+++ src/student/agentDetail/agentDetail.php
@@ -1,15 +1,30 @@
 <?php
 require "../../dbconnect.php";
+$age_chose=$_GET['detail'];
 
-$agent_data="select * from agent_info;";
+$agent_data = "select * from agent_info;";
+$agent_det="select * from agent_info where name='$age_chose';";
+$agent_tag1="select * from tags inner join info_tags on tags.id=info_tags.tag_id where agent_name = '$age_chose' and tag_id between 0 and 11;";
+$agent_tag2="select * from tags inner join info_tags on tags.id=info_tags.tag_id where agent_name = '$age_chose' and tag_id between 12 and 17;";
+$agent_tag3="select * from tags inner join info_tags on tags.id=info_tags.tag_id where agent_name = '$age_chose' and tag_id between 18 and 24;";
 
-$agent_infos=$db->query($agent_data)->fetchAll(PDO::FETCH_ASSOC);
+$agent_infos = $db->query($agent_data)->fetchAll(PDO::FETCH_ASSOC);$stu_chose=$_GET['detail'];
+$agent_details=$db->query($agent_det)->fetchAll(PDO::FETCH_ASSOC);
+$first_tags=$db->query($agent_tag1)->fetchAll(PDO::FETCH_ASSOC);
+$second_tags=$db->query($agent_tag2)->fetchAll(PDO::FETCH_ASSOC);
+$third_tags=$db->query($agent_tag3)->fetchAll(PDO::FETCH_ASSOC);
+
+
+
+
+
+
 
 
 ?>
-
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -96,50 +111,70 @@ $agent_infos=$db->query($agent_data)->fetchAll(PDO::FETCH_ASSOC);
         <!-- modal ここまで -->
     </header>
     <main>
+        <?php foreach($agent_details as $agent_detail):?>
         <article class="detail_wrapper">
             <section class="detail_top">
-                <div class="detail_agent_name">リクナビ</div>
-                <img src="../../materials/rikunavi.png" alt="リクナビ">
+                <div class="detail_agent_name"><?=$agent_detail['name']?></div>
+                <img src="../../materials/<?=$agent_detail['image']?>" alt="<?=$agent_detail['name']?>">
                 <p>2023年卒業予定の新卒の皆様と既卒者を対象とした学生と企業を結び付ける、新卒採用の就職情報サイトです。</p>
             </section>
             <section class="detail_bottom">
-                <div class="detail_table_title">ー　基本情報　ー</div>
+                <div class="detail_table_title">ー 基本情報 ー</div>
                 <table class="detail_table">
                     <tr>
                         <td>エージェント名</td>
-                        <td>リクナビ</td>
+                        <td><?=$agent_detail['name']?></td>
                     </tr>
                     <tr>
                         <td>主な業界</td>
-                        <td>ITコンサル</td>
+                        <td>
+                            <?php foreach($first_tags as $first_tag):
+                                echo $first_tag['tag'];
+                                echo ",";
+                            endforeach;
+                            ?>
+                        </td>
                     </tr>
                     <tr>
                         <td>拠点地域</td>
-                        <td>東京</td>
+                        <td>
+                        <?php foreach($second_tags as $second_tag):
+                            echo $second_tag['tag'];
+                            echo ",";
+
+                        endforeach;
+                        ?>
+
+                        </td>
                     </tr>
                     <tr>
                         <td>保有求人数</td>
-                        <td>300社</td>
+                        <td><?=$agent_detail['Num_of_firm']?></td>
                     </tr>
                     <tr>
                         <td>内定率</td>
-                        <td>80%</td>
+                        <td><?=$agent_detail['offer_rate']?>%</td>
                     </tr>
                     <tr>
                         <td>利用人数</td>
-                        <td>2万人</td>
+                        <td><?=$agent_detail['population']?>人</td>
                     </tr>
                     <tr>
                         <td>面談形態</td>
-                        <td>オンライン</td>
-                    </tr>
-                    <tr>
-                        <td>応募者数上限</td>
-                        <td>25社</td>
+                        <td>
+                            <?php if($agent_detail['interview_support']=1){
+                                echo "オンライン";
+                            }else{
+                                echo "オフライン";
+                                
+                            }
+                                ?>
+                        </td>
                     </tr>
                 </table>
             </section>
         </article>
+        <?php endforeach;?>
     </main>
     <footer>
         <ul>
@@ -154,4 +189,5 @@ $agent_infos=$db->query($agent_data)->fetchAll(PDO::FETCH_ASSOC);
     </footer>
     <script src=""></script>
 </body>
+
 </html>
